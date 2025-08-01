1ff2307fb0c9c5f45f3adc0937a07a251cb8c24c
Tharsan01 <1211saran@gmail.com>
2025-08-01 13:25:27 +0530

    Enhance tab layout with screen options and styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
-import {Ionicons} from '@expo/vector-icons'
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import React from "react";
 
 const TabLayout = () => {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Todos",tabBarIcon:({color,size})=>(<Ionicons name='flash-outline' size={size} color={color}/>) }} />
-      <Tabs.Screen name="settings" options={{ title: "Settings", tabBarIcon:({color,size})=>(<Ionicons name='settings' color={color} size={size}/>) }} />
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: "#1e293b",
+          borderTopWidth: 1,
+          borderTopColor: "yellow",
+          height: 90,
+          paddingBottom: 30,
+          paddingTop: 10,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "600",
+        },
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Todos",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="flash-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" color={color} size={size} />
+          ),
+        }}
+      />
     </Tabs>
   );
-}
+};
 
-export default TabLayout
+export default TabLayout;
